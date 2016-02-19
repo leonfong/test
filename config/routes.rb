e8e6a31d98648bcf30dcd7335eaa6ac4bc5f9ee2
leon <88554938@qq.com>
2016-02-19 14:51:29 +0800

    20160219-1451

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   match '/search', to: 'boms#search_keyword', via: 'get'
   match '/down_excel', to: 'boms#down_excel', via: 'get'
   match '/work_flow', to: 'work_flow#index', via: 'post'  
+  match '/work_flow', to: 'work_flow#index', via: 'get'  
   match '/up_work', to: 'work_flow#up_work', via: 'post'
   match '/edit_work', to: 'work_flow#edit_work', via: 'post'
 

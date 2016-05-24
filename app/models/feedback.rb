370d772ef4e87a494f990099fff53c865f3e75e5
leon <88554938@qq.com>
2016-05-24 10:21:12 +0800

    up 20160524-1021

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -6,7 +6,7 @@ class Feedback < ActiveRecord::Base
             Rails.logger.info("qwqwqwqwqwqwqwqwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
             Rails.logger.info(self.topic_id.inspect)
             Rails.logger.info("qwqwqwqwqwqwqwqwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
-            open_id = User.find_by(email: (Topic.find_by(self.topic_id).user_name)).open_id
+            open_id = User.find_by(email: (Topic.find(self.topic_id).user_name)).open_id
             oauth = Oauth.find(1)
             company_id = oauth.company_id
             company_token = oauth.company_token
@@ -20,7 +20,7 @@ class Feedback < ActiveRecord::Base
                 url += '&to_all=0'  
                 url += '&receivers='+open_id
                 url += '&window_title=Fastbom-PCB AND PCBA'
-                url += '&tips_title='+URI.encode('亲爱的'+User.find_by(email: (Topic.find_by(self.topic_id).user_name)).full_name)
+                url += '&tips_title='+URI.encode('亲爱的'+User.find_by(email: (Topic.find(self.topic_id).user_name)).full_name)
                 url += '&tips_content='+URI.encode('你有新的回复，点击查看。')
                 url += '&tips_url=www.fastbom.com/feedback?id='+self.topic_id.to_s 
                 resp = Net::HTTP.get_response(URI(url))

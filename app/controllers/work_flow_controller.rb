221f33fddbfbaf5341382470f6522ec908a2c2b0
leon <88554938@qq.com>
2016-06-14 09:51:55 +0800

    up 201606140952

--- app/controllers/work_flow_controller.rb
+++ app/controllers/work_flow_controller.rb
@@ -341,27 +341,27 @@ before_filter :authenticate_user!
                         Rails.logger.info("qwqwqwqwqwqwqwqwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww")
 
                         open_id = User.find_by(s_name: (work_history.order_no.split("-")[0].scan(/\D/).join("").chop.delete("mk").delete("MK"))).open_id
-                        oauth = Oauth.find(1)
-                        company_id = oauth.company_id
-                        company_token = oauth.company_token
-                        url = 'https://openapi.b.qq.com/api/tips/send'
-                        if not open_id.blank? 
-                            url += '?company_id='+company_id
-                            url += '&company_token='+company_token
-                            url += '&app_id=200710667'
-                            url += '&client_ip=120.25.151.208'
-                            url += '&oauth_version=2'
-                            url += '&to_all=0'  
-                            url += '&receivers='+open_id
-                            url += '&window_title=Fastbom-PCB AND PCBA'
-                            url += '&tips_title='+URI.encode('亲爱的'+User.find_by(s_name: (work_history.order_no.split("-")[0].scan(/\D/).join("").chop.delete("mk").delete("MK"))).full_name)
-                            url += '&tips_content='+URI.encode('你的订单入库数量有更新，点击查看。')
+                        if not open_id.blank?
+                            oauth = Oauth.find(1)
+                            company_id = oauth.company_id
+                            company_token = oauth.company_token
+                            url = 'https://openapi.b.qq.com/api/tips/send'
+                            if not open_id.blank? 
+                                url += '?company_id='+company_id
+                                url += '&company_token='+company_token
+                                url += '&app_id=200710667'
+                                url += '&client_ip=120.25.151.208'
+                                url += '&oauth_version=2'
+                                url += '&to_all=0'  
+                                url += '&receivers='+open_id
+                                url += '&window_title=Fastbom-PCB AND PCBA'
+                                url += '&tips_title='+URI.encode('亲爱的'+User.find_by(s_name: (work_history.order_no.split("-")[0].scan(/\D/).join("").chop.delete("mk").delete("MK"))).full_name)
+                                url += '&tips_content='+URI.encode('你的订单入库数量有更新，点击查看。')
                             #url += '&tips_url=www.fastbom.com/feedback?id='+self.topic_id.to_s 
-                            url += '&tips_url=www.fastbom.com/work_flow?utf8=%E2%9C%93%26order_s%5Border_s%5D=1%26order=' + work_history.order_no + '%26commit=%E6%90%9C%E7%B4%A2'
-                            resp = Net::HTTP.get_response(URI(url))
-                
-                        end                    
-    
+                                url += '&tips_url=www.fastbom.com/work_flow?utf8=%E2%9C%93%26order_s%5Border_s%5D=1%26order=' + work_history.order_no + '%26commit=%E6%90%9C%E7%B4%A2'
+                                resp = Net::HTTP.get_response(URI(url))
+                            end                    
+                        end
                     end
                 else
                     redirect_to work_flow_path, :flash => {:error => item+"--------入库数量更新失败，请检查上传数据格式！"}

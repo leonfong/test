6c37b827fd089b8306ab99b4ae1b484c3e1bff22
leon <88554938@qq.com>
2016-09-24 10:11:59 +0800

    up 2016092241012

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -47,6 +47,7 @@ class Ability
       can :work_g_all, :all
       can :work_g, :all
       can :work_e, :all
+      can :work_d, :all
       can :external_access, :all
       can :work_pcb_business, :all
       can :work_suppliers, :all
